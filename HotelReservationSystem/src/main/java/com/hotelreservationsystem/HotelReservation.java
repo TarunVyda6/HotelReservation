ed3b73d2ef0c88450e0fabc73d2bfaf672519a73
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-01 12:29:09 +0530

    Added : Feature to find the cheapest hotel in week days

--- HotelReservationSystem/src/main/java/com/hotelreservationsystem/HotelReservation.java
+++ HotelReservationSystem/src/main/java/com/hotelreservationsystem/HotelReservation.java
@@ -1,21 +1,59 @@
 package com.hotelreservationsystem;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Hotel Reservation Program");
+	Hotel LakeWood = new Hotel("Lakewood", 110);
+	Hotel BridgeWood = new Hotel("Bridgewood", 160);
+	Hotel RidgeWood = new Hotel("Ridgewood", 220);
 
-		Hotel LakeWood = new Hotel("Lakewood", 110);
-		Hotel BridgeWood = new Hotel("Bridgewood", 160);
-		Hotel RidgeWood = new Hotel("Ridgewood", 220);
+	List<Hotel> hotelList = new ArrayList<Hotel>();
 
-		List<Hotel> hotelList = new ArrayList<Hotel>();
+	/**
+	 * adds total hotels available in the city to hotel list
+	 */
+	public HotelReservation() {
+		super();
 		hotelList.add(LakeWood);
 		hotelList.add(BridgeWood);
 		hotelList.add(RidgeWood);
 
+	}
+
+	/**
+	 * @param checkInDate
+	 * @param checkOutDate
+	 * @return cheapest hotel during week days
+	 */
+	public Hotel cheapestHotelOfWeekDayRates(String checkInDate, String checkOutDate) {
+
+		int noOfDays = getTotalDays(checkInDate, checkOutDate);
+		List<Integer> price = hotelList.parallelStream().map(hotel -> hotel.getWeekDayRatesForRegular() * noOfDays)
+				.collect(Collectors.toList());
+		int minPrice = Collections.min(price);
+		return hotelList.stream().filter(hotel -> hotel.getWeekDayRatesForRegular() * noOfDays == minPrice).findFirst()
+				.orElse(null);
+
+	}
+
+	/**
+	 * @param checkInDate
+	 * @param checkOutDate
+	 * @return total no of days between check in and check out
+	 */
+	private int getTotalDays(String checkInDate, String checkOutDate) {
+
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		LocalDate checkInLocalDate = LocalDate.parse(checkInDate, dateTimeFormatter);
+		LocalDate checkOutLocalDate = LocalDate.parse(checkOutDate, dateTimeFormatter);
+		int noOfDays = (int) ChronoUnit.DAYS.between(checkInLocalDate, checkOutLocalDate);
+		return noOfDays + 1;
 	}
 }
--- HotelReservationSystem/src/test/java/com/hotelreservationsystem/HotelReservationTest.java
+++ HotelReservationSystem/src/test/java/com/hotelreservationsystem/HotelReservationTest.java
@@ -1,5 +1,25 @@
 package com.hotelreservationsystem;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Tarun vyda
+ *
+ */
 public class HotelReservationTest {
+
+	/**
+	 * checks for the cheapest hotel between given date range of week days and
+	 * returns the cheapest hotel
+	 */
+	@Test
+	public void givenDateRangeOfWeekDays_WhenAnalyse_ShouldReturnCheapestHotelName() {
+
+		HotelReservation hotelReservation = new HotelReservation();
+		Hotel cheapestHotel = hotelReservation.cheapestHotelOfWeekDayRates("10Sep2020", "11Sep2020");
+		Assert.assertEquals("Lakewood", cheapestHotel.getName());
+
+	}
 
 }
